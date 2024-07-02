4202270983b1a7d582466bcaa0896dc4cee79852
OpenVAET <openvaet@protonmail.com>
2024-07-03 00:29:10 +0200

    Birth Rates - v 1.0.3 - AT Women in age

---
+++ country/at/women_in_age.R
@@ -0,0 +1,36 @@
+library(ggplot2)
+library(readr)
+library(tidyr)
+
+# Reads the CSV file
+file_path <- "data/at/20240702_23_21_women_age_group_2013_2023.csv"
+data <- read_csv(file_path)
+
+# Reshapes the data to long format for plotting
+data_long <- pivot_longer(data, cols = -Age, names_to = "Year", values_to = "Count")
+
+# Converts Year to integer
+data_long$Year <- as.integer(data_long$Year)
+
+# Creates plot
+data_long$Year <- as.integer(data_long$Year)
+ggplot(data_long, aes(x = Year, y = Count, color = Age)) +
+  geom_line(size = 1.2) +
+  geom_point(size = 3) +
+  labs(title = "Evolution of Women by Age Group (2013-2023)",
+       x = "Year",
+       y = "Number of Women",
+       color = "Age Group") +
+  theme_minimal() +
+  scale_x_continuous(breaks = 2013:2023) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, max(data_long$Count) + 10000)) +
+  theme(
+    text = element_text(size = 18),
+    plot.title = element_text(size = 22, face = "bold"),
+    axis.title = element_text(size = 20),
+    axis.text = element_text(size = 18),
+    legend.title = element_text(size = 20),
+    legend.text = element_text(size = 18),
+    plot.caption = element_text(size = 16)
+  )
+
