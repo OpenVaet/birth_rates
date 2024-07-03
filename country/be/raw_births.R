8b595af8ed87f9d6e626eabbc5d9df9a9ae38722
OpenVAET <openvaet@protonmail.com>
2024-07-03 03:50:20 +0200

    Birth Rates - v 1.0.5 - BE raw births

--- country/be/raw_births.R
+++ country/be/raw_births.R
@@ -1,15 +1,16 @@
-# Loads required libraries
 library(ggplot2)
+library(scales)
 
 # Births data
 years <- 2013:2023
-births <- c(79330, 81722, 84381, 87675, 87633, 85535, 84952, 83603, 86078, 82627, 77605)
+births <- c(124862, 124415, 121713, 121161, 119102, 117800, 117103, 113739, 117914, 113593, 110198)
 
 data <- data.frame(year = years, births = births)
+data$births <- as.integer(data$births)
 
 print(data)
 # Prints the yearly births.
-write.csv(data, 'data/at/births_by_year.csv')
+write.csv(data, 'data/be/births_by_year.csv')
 
 # Fits a linear model to data from 2013 to 2019
 fit <- lm(births ~ year, data = data[data$year <= 2019,])
@@ -27,13 +28,13 @@ ggplot(data, aes(x = year, y = births)) +
   geom_line(aes(y = predicted), linetype = "dashed", color = "blue", size = 1.2) +
   geom_text(aes(label = ifelse(year >= 2020, paste0(round(deviation, 1), "%"), "")),
             vjust = -1, size = 6) +
-  labs(title = "AT - Births and Deviation from Linear Trend (2013-2019)",
+  labs(title = "BE - Births and Deviation from Linear Trend (2013-2019)",
        x = "Year",
        y = "Number of Births",
        caption = "Dashed line represents the linear trend (2013-2019)") +
   theme_minimal() +
-  ylim(0, max(data$births) + 5000) +
   scale_x_continuous(breaks = years) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, max(data$births) + 5000)) +
   theme(
     text = element_text(size = 18),
     plot.title = element_text(size = 22, face = "bold"),
@@ -43,4 +44,3 @@ ggplot(data, aes(x = year, y = births)) +
     legend.text = element_text(size = 18),
     plot.caption = element_text(size = 16)
   )
-
